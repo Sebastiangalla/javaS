b07d5c713172349d5f3684d957198e2ea69258f8
Sebastiangalla <87157476+Sebastiangalla@users.noreply.github.com>
2022-07-27 20:57:55 -0300

    Add files via upload

--- desafio entregable/ciclos1.js
+++ desafio entregable/ciclos1.js
@@ -1,7 +1,7 @@
-let numero=parseInt(prompt("Ingresa un numero (-1 para salir)"));
+let numero=prompt("Ingresa un numero (ESC para salir)");
 let sumaNumeros=0;
-while(numero!= -1){
-    sumaNumeros=sumaNumeros+numero;
+while(numero.toLowerCase() != "esc"){
+    sumaNumeros=sumaNumeros+ parseInt(numero);
     console.log("la suma de numeros es de:" + Number(sumaNumeros))
-    numero=parseInt(prompt("Ingresa un numero (-1 para salir)"));
+    numero=prompt("Ingresa un numero (ESC para salir)");
 }
